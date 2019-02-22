db19291cbc4d7f1b05532630569f880dc5952051
tTeha <tteha43@gmail.com>
2019-02-22 19:39:11 +0200

    .

--- Implementation/EHR_system/ehr/ehr/urls.py
+++ Implementation/EHR_system/ehr/ehr/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from App_1 import views as view
+from App_1 import views as view
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
